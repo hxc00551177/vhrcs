88e2e3fd62a6b777a7568d1ca44f2210fe1bc994
hxc00551177 <2102527169@qq.com>
2020-08-03 12:52:30 +0800

    boot调度

---
+++ src/main/java/org/aoligei/vhrcs/VhrcsApplication.java
@@ -0,0 +1,13 @@
+package org.aoligei.vhrcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhrcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhrcsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/aoligei/vhrcs/VhrcsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aoligei.vhrcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhrcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
